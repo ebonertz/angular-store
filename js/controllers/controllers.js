4d0d60f67cad567b94247bb2a0e3928e8e1c9d14
Jeremy Smith <smithjeremym@gmail.com>
2016-03-18 14:37:04 -0400

    get json call working

--- js/controllers/controllers.js
+++ js/controllers/controllers.js
@@ -1,10 +1,22 @@
-IronTix.controller('MainController', ['$scope', function($scope) {
-  $scope.title = 'Upcoming Events';
-  $scope.promo = 'Awesome Event You Love!';
-  $scope.product = {
-  	name: 'Maywood',
-    location:'Raleigh',
-    price: '$8.99',
-    date:'March 18th'
-};
+IronTix.controller('MainController', ['$http', '$scope', function($http, $scope) {
+  // $scope.title = 'Upcoming Events';
+  // $scope.promo = 'Awesome Event You Love!';
+  // $scope.product = {
+  // 	name: 'Maywood',
+  //   location:'Raleigh',
+  //   price: '$8.99',
+  //   date:'March 18th'
+
+  // below is an alternative way to call our json data
+  // $http.get('events.json')
+  // .then(function(response){
+  //   $scope.event = response.data;
+  //   console.log("shit!");
+  //   console.log(event.events[0]);
+  // });
+
+  $http.get('events.json').success(function(data){
+    console.log("json has been successfully called");
+    $scope.event = data;
+  });
 }]);
