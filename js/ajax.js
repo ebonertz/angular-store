61cc8c68c55dec5275f5c54c2cb53d62570e0614
Jeremy Smith <smithjeremym@gmail.com>
2016-03-20 16:17:04 -0400

    add quantity adjusters onto cart items, start building out jquery to handle that logic

--- js/ajax.js
+++ js/ajax.js
@@ -4,7 +4,7 @@ $(document).ready( function () {
 
 // this eventlistener adds the correct item to the cartEvents array in the database
   $("body").on("click", ".fa-cart-plus", function () {
-    console.log($(this).siblings(".storefrontID").html());
+    // console.log($(this).siblings(".storefrontID").html());
     // get the ID of the event clicked so we can GET the info for it to display in the cart
     var clickedEventID = $(this).siblings(".storefrontID").html();
 
@@ -30,21 +30,22 @@ $(document).ready( function () {
                             contentType: "application/json",
                           },
                         success : function(data) {
+                            console.log("you've added the following thing to cartEvents:")
                             console.log(data);
                             // this innermost GET function is not working
-                            $.ajax({
-                                    type : 'GET',
-                                    dataType : 'json',
-                                    url: 'http://localhost:3000/',
-                                    headers: {
-                                        contentType: "application/json",
-                                      },
-                                    success : function(data) {
-                                        console.log("the innermost function worked");
-                                      }, error: function(request,error){
-                                        alert("Request: " +JSON.stringify(request));
-                                      }
-                              });
+                            // $.ajax({
+                            //         type : 'GET',
+                            //         dataType : 'json',
+                            //         url: 'http://localhost:3000/',
+                            //         headers: {
+                            //             contentType: "application/json",
+                            //           },
+                            //         success : function(data) {
+                            //             console.log("the innermost function worked");
+                            //           }, error: function(request,error){
+                            //             alert("Request: " +JSON.stringify(request));
+                            //           }
+                            //   });
                         }, error: function(request,error){
                           alert("Request: " +JSON.stringify(request));
                           console.log(clickedEventID);
@@ -58,9 +59,10 @@ $(document).ready( function () {
 
   });
 
+  // this eventListener deletes the item from the cart
   $("body").on("click", ".fa-times", function () {
-    console.log($(this).siblings(".storefrontID").html());
-    // get the ID of the event clicked so we can GET the info for it to display in the cart
+    // console.log($(this).siblings(".storefrontID").html());
+    // get the ID of the event clicked so we can GET the info for it to be removed from the cart
     var clickedEventID = $(this).siblings(".storefrontID").html();
 
     $.ajax({
@@ -107,5 +109,24 @@ $(document).ready( function () {
   //         }
   // });
 
+  // these clickEvents should update the quantity for each cart item and then update the order total on the cart
+  var currentQuantity;
+  $("body").on("click", ".fa-minus-square", function () {
+    console.log($(this).siblings(".lineItem-quantity").html());
+    currentQuantity = $(this).siblings(".lineItem-quantity").html();
+  });
 
+  $("body").on("click", ".fa-plus-circle", function () {
+    console.log($(this).siblings(".lineItem-quantity").html());
+    currentQuantity = $(this).siblings(".lineItem-quantity").html();
+    if (currentQuantity <= 9){
+      //the line below is actually just throwing a '1' after the first number. I need to get the code to think that currentQuantity is a number !
+      $(this).siblings(".lineItem-quantity").html(currentQuantity + 1);
+      console.log(currentQuantity);
+      currentQuantity =+ 1;
+      event.stopPropagation();
+    } else {
+      // do nothing - you can only buy 9 tickets for a single event
+    }
+  });
 });
