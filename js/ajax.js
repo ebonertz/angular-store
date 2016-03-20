a541914633749c1fdf7d9af2d0606c808827c3ca
Jeremy Smith <smithjeremym@gmail.com>
2016-03-20 15:15:24 -0400

    add delete item functionality to cart items

--- js/ajax.js
+++ js/ajax.js
@@ -2,7 +2,7 @@ $(document).ready( function () {
 
 // var cartData;
 
-
+// this eventlistener adds the correct item to the cartEvents array in the database
   $("body").on("click", ".fa-cart-plus", function () {
     console.log($(this).siblings(".storefrontID").html());
     // get the ID of the event clicked so we can GET the info for it to display in the cart
@@ -31,6 +31,20 @@ $(document).ready( function () {
                           },
                         success : function(data) {
                             console.log(data);
+                            // this innermost GET function is not working
+                            $.ajax({
+                                    type : 'GET',
+                                    dataType : 'json',
+                                    url: 'http://localhost:3000/',
+                                    headers: {
+                                        contentType: "application/json",
+                                      },
+                                    success : function(data) {
+                                        console.log("the innermost function worked");
+                                      }, error: function(request,error){
+                                        alert("Request: " +JSON.stringify(request));
+                                      }
+                              });
                         }, error: function(request,error){
                           alert("Request: " +JSON.stringify(request));
                           console.log(clickedEventID);
@@ -43,20 +57,40 @@ $(document).ready( function () {
     event.stopPropagation();
 
   });
-  // the thing below DELETEs the object with ID=X in the cartEvents array
-  $.ajax({
-          type : 'DELETE',
-          dataType : 'json',
-          url: 'http://localhost:3000/cartEvents/X/',
-          headers: {
-              contentType: "application/json",
-            },
-          success : function(data) {
-              console.log("some stuff was deleted!");
-          }, error: function(request,error){
-            alert("Request: " +JSON.stringify(request));
-          }
+
+  $("body").on("click", ".fa-times", function () {
+    console.log($(this).siblings(".storefrontID").html());
+    // get the ID of the event clicked so we can GET the info for it to display in the cart
+    var clickedEventID = $(this).siblings(".storefrontID").html();
+
+    $.ajax({
+            type : 'DELETE',
+            dataType : 'json',
+            url: 'http://localhost:3000/cartEvents/'+clickedEventID+'/',
+            headers: {
+                contentType: "application/json",
+              },
+            success : function(data) {
+                console.log("the event with id " + clickedEventID+ " was removed from the cart!");
+            }, error: function(request,error){
+              alert("Request: " +JSON.stringify(request));
+            }
+    });
   });
+  // the thing below DELETEs the object with ID=X in the cartEvents array
+  // $.ajax({
+  //         type : 'DELETE',
+  //         dataType : 'json',
+  //         url: 'http://localhost:3000/cartEvents/X/',
+  //         headers: {
+  //             contentType: "application/json",
+  //           },
+  //         success : function(data) {
+  //             console.log("some stuff was deleted!");
+  //         }, error: function(request,error){
+  //           alert("Request: " +JSON.stringify(request));
+  //         }
+  // });
 
   // I want the thing below to DELETEs everything in the cartEvents array, but it doesn't work!
   // $.ajax({
